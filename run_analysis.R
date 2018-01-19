407cfe0e8e8cefcb101cf0da267272f2a5022c31
httusg <httusg@gmail.com>
2018-01-19 16:39:23 +0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -59,7 +59,7 @@ colnames(std_measurements)  = slist_clean
 #5.From the data set in step 4, creates a second, independent tidy data set
 #  with the average of each variable for each activity and each subject.
 #
-# subject (subject) - activity (y) - var (mean_measurements, std_measurements)
+# subject (subject) - activity (y) - measurements (mean_measurements, std_measurements)
 ####################################################################
 activity <- y$activity
 SubActMeas <- cbind(subject, activity, mean_measurements, std_measurements)
